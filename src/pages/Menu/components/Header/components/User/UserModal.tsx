7153f7f8ed1d99309ddb0abc2bc524d358f3405c
Julio Ricardo <juliomilcharek519@gmail.com>
2024-01-16 11:11:29 -0300

    finalize UserModal component

--- src/pages/Menu/components/Header/components/User/UserModal.tsx
+++ src/pages/Menu/components/Header/components/User/UserModal.tsx
@@ -14,9 +14,38 @@ export const UserModal = () => {
                             children="Faça seu Login"
                             style={{width: `21.8rem`}}
                         />
-                        <div className="link">
-                            <p>Cliente novo?</p> <a> <Link to="/signup">Comeque aqui</Link></a>
-                        </div>
+                            <p>Cliente novo?</p> <Link to="/signup"><a>Comece aqui</a></Link>
+                            <hr className="horizontal-line"/>
+
+                            <div className="account-pref">
+                                <div className="list">
+                                    <h2>Suas Listas</h2>
+                                    <a className="link-modal">Criar uma Lista de desejos</a>
+                                </div>
+
+                                <div>
+                                    <hr className="vertical-line"/>
+                                </div>
+
+                                <div className="list-tools">
+                                    <h2>Sua Conta</h2>
+                                    <ul>
+                                        <li><a href="#" className="link-modal">Sua conta</a></li>
+                                        <li><a href="#" className="link-modal">Seus pedidos</a></li>
+                                        <li><a href="#" className="link-modal">Sua Lista de desejos</a></li>
+                                        <li><a href="#" className="link-modal">Recomendados para você</a></li>
+                                        <li><a href="#" className="link-modal">Programe e Poupe</a></li>
+                                        <li><a href="#" className="link-modal">Sua assinatura Prime</a></li>
+                                        <li><a href="#" className="link-modal">Incrições e assinaturas</a></li>
+                                        <li><a href="#" className="link-modal">Gerencie seu conteúdo e <br/>dispositivos</a></li>
+                                        <li><a href="#" className="link-modal">Seu Prime Video</a></li>
+                                        <li><a href="#" className="link-modal">Seu Kindle Unlimited</a></li>
+                                        <li><a href="#" className="link-modal">Seu Amazon Drive em <br/>Amazon.com</a></li>
+                                        <li><a href="#" className="link-modal">Seus aplicativos e dispositivos</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            
                     </div>
                 </div>
             </div>
--- src/pages/Menu/components/Header/components/User/style.ts
+++ src/pages/Menu/components/Header/components/User/style.ts
@@ -28,7 +28,6 @@ export const Container = styled.div`
         font-weight: 600;
         display: inline;
     }
-//1113px
     .triangle-modal {
         display: none;
         position: absolute;
@@ -60,14 +59,16 @@ export const Container = styled.div`
 
 
     .modal-content {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
+        display: block;
+        text-align: center;
         
+        Button {
+            margin-bottom: 5px;
+        } 
+
         p {
+            display: inline;
             color: #747475;
-            padding-top: 5px;
             font-size: 1rem;
         }
         
@@ -77,10 +78,69 @@ export const Container = styled.div`
             text-decoration: none;
         }
         
-        
         a:hover {
             color: var(--link-color-hover);
             text-decoration: underline;
+        }
+
+        .horizontal-line {
+            width: 90%;
+            margin-left: 25px;
+            margin-top: 1rem;
+            border: none;
+            height: 1px;
+            background: #e6e6e6;
+        }
+
+        .list, .list-tools {
+            color: #000000;
+        }
+
+        .account-pref {
+            display: flex;
+            justify-content: center;
+            gap: 2rem;    
+            text-align: start;
+            margin-top: 10px;
+        }
+        .list {
+            margin-right: 90px;
+            a {
+                font-size: 1.2rem;
+            }
+        }
+        .list-tools {
+            h2, ul {
+                display: block;
+            }
+
+            ul {
+                list-style: none;
+                line-height: 2;
+                li:nth-child(8), li:nth-child(11) {
+                    line-height: 1;
+                }
+                a {
+                    font-size: 1.3rem;
+                }
+                
+            }
+        }
+        .link-modal {
+            font-weight: 400;
+            font-size: 1.1rem;
+            color: #6f777c;
+        }
+
+        .link-modal:hover {
+            color: var(--link-color-hover);
+        }
+
+        .vertical-line {
+            width: 1px;
+            height: 100%;
+            border-left: 1px solid;
+            border-top-color: transparent;
         }
     }
 `;
