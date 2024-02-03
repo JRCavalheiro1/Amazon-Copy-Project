32a72e916e5c02cf29b6444bad5a8e51288f46f0
Julio Ricardo <juliomilcharek519@gmail.com>
2024-02-03 10:04:54 -0300

    edit some things at poster component

--- src/pages/Menu/components/Hero/Components/Poster/Poster.tsx
+++ src/pages/Menu/components/Hero/Components/Poster/Poster.tsx
@@ -58,17 +58,14 @@ export const Poster = ({image} : posterProps) => {
                             initial="enter"
                             animate="center"
                             exit="exit"
-                            transition={{
-                                x: {type: "spring", stiffness: 300, damping: 30},
-                                opacity: {duration: 0.2}
-                            }}
+                           
                             drag="x"
                             onDragEnd={(e, {offset, velocity}) => {
                                 const swipe = swipePower(offset.x, velocity.x);
                                  
-                                if (swipe < -swipeConfidenceThreshold) {
+                                if (swipe > -swipeConfidenceThreshold) {
                                     paginate(1);
-                                } else if (swipe > swipeConfidenceThreshold) {
+                                } else if (swipe < swipeConfidenceThreshold) {
                                     paginate(-1);
                                 }
 
