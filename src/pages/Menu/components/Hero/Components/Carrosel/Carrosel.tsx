338e49bffac95d1ba4f58e951f075fa2a5f8bd18
Julio Ricardo <juliomilcharek519@gmail.com>
2024-02-16 11:10:24 -0300

    added onDraggin function at carrosel component

--- src/pages/Menu/components/Hero/Components/Carrosel/Carrosel.tsx
+++ src/pages/Menu/components/Hero/Components/Carrosel/Carrosel.tsx
@@ -3,6 +3,7 @@ import { Container } from "./style";
 import prevBtn from "../../../../../../images/prev-btn.svg";
 import nextBtn from "../../../../../../images/next-btn.svg";
 import { useRef, useState, useEffect } from "react";
+import React from "react";
 
 type carroselProps = {
     data?: Array<any> | undefined,
@@ -19,33 +20,62 @@ const variants = {
             x: direction
         }
     },
-   
 }
+
+const variantsDragBar = {
+    initial: {
+        x: 0,
+    },
+    animate: (direction: number) => {
+        return {
+            x: direction
+        }
+    }
+}
+
 
 
 
 //x 0 - -3140
 export const Carrosel = ({h1, data} : carroselProps) => {
     const [control, setControl] = useState(0);
+    const [isDrag, setIsDrag] = useState(false);
+    const [dragDirection, setDragDirection] = useState(0);    
+
+    const carouselref = React.createRef<HTMLDivElement>();
+    const carouselWidth = carouselref.current?.offsetWidth;
+
     const scrollBarRef = useRef<HTMLDivElement>(null);
 
-    const sizes = [1250, 1250, 800];
-    const nextRef = useRef<HTMLDivElement>(null);
-    const prevRef = useRef<HTMLDivElement>(null);
-    
-    
     const next = () => {
-        setControl(control + 1100);
-        if(control >= 3300) {
-            setControl(3300);
+        setControl(control + 1110);
+        if(control >=  (carouselref.current ? carouselref.current.offsetWidth : 0)) {
+            setControl(3330);
         }
-        
     }   
+
     const prev = () => {
-        setControl(control - 1100);
-        if(control <= 0) {
+        setControl(control - 1110);
+        if(control <= (carouselref.current ? carouselref.current.offsetWidth : 0)) {
             setControl(0);
         }
+    }
+    
+
+    const onDragStart = () => {
+        setControl(control);
+        setIsDrag(true)
+    }
+    const onDraggin = (e: any) => {
+       isDrag ? setControl((e.clientX * 0.25)) : undefined
+       if(control >=  (carouselref.current ? carouselref.current.offsetWidth : 0)){
+        setControl(3300);
+       }
+    }
+    
+    const onDragEnd = () => {
+        setControl(control)
+        setIsDrag(false)
     }
     
     return (
@@ -54,7 +84,7 @@ export const Carrosel = ({h1, data} : carroselProps) => {
                 <h1>{h1}</h1>
                 
 
-                <div className="carrousel">
+                <div className="carrousel" ref={carouselref}>
                     <motion.div
                         variants={variants}
                         custom={-control}
@@ -84,13 +114,17 @@ export const Carrosel = ({h1, data} : carroselProps) => {
 <div className="drag-section" ref={scrollBarRef}>
                     <motion.div
                         drag="x"
-                        animate={{x: control / 3}}
+                        variants={variantsDragBar}
+                        animate="animate"
                         transition={{spring: 0, duration: 0.35}}
                         dragConstraints={scrollBarRef}
                         dragElastic={0}
                         dragMomentum={false}
                         className="drag-bar"
                         draggable
+                        onDragStart={()=> onDragStart()}
+                        onDrag={(e)=> onDraggin(e)}
+                        onDragEnd={()=> onDragEnd()}
                         >
                     </motion.div>
                 </div>
