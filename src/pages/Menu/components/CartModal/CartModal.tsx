f67bd8233be099f0237e94e4f6a2d1a8e2edcf6a
Julio Ricardo <juliomilcharek519@gmail.com>
2024-04-10 10:50:55 -0300

    added style to cart modal component

--- src/pages/Menu/components/CartModal/CartModal.tsx
+++ src/pages/Menu/components/CartModal/CartModal.tsx
@@ -7,8 +7,8 @@ type CartModalProps = {
 }
 const variants = {
     visible: {x: 0, opacity: 1},
-    initial: {x: "-100vh", opacity: 0},
-    exit: {x: "-100vh", opacity: 0}
+    initial: {x: "100vh", opacity: 0},
+    exit: {x: "100vh", opacity: 0}
     
 }
 
@@ -16,15 +16,44 @@ export const CartModal = ({isOpen} : CartModalProps) => {
     return (
             <Container>
                 <ComponentAnimation
+                    key="cart-modal"
+                    style={{top: 0, position: "absolute", right: 0}}
                     isOpen={true}
                     variants={variants}
                     initial="initial"
                     animate="visible"
                     exit="exit"
+                    transition={{duration: 0.4}}
                     children={
                         <div className="cart-modal">
-                            karalhos
-    
+                            <div className="cart-m-total-cost">
+                                <span>Subtotal</span>
+                                <h2>R$ 498,00</h2>
+                            </div>
+                            <div className="free-delivery">
+                                <p id="normal-p">
+                                   <p id="green-p"> Seu pedido se qualifica para <b>Frete GRÁTIS</b>.</p> 
+                                   Selecione Frete GRÁTIS ao finalizar o pedido.<br/> <a id="link-p" href="#">Veja detalhes.</a>
+                                </p>
+                            </div>
+                            <div className="go-to-cart-page">
+                                <button>Ir para o carrinho</button>
+                            </div>
+
+                            <div className="product">
+                                <div className="product-visualize">
+                                        <div className="product-img">
+                                            <img/>
+                                        </div>
+                                        <div className="product-cost"> 
+                                            <h2>R$ 286,00</h2>
+                                        </div>
+
+                                        <div className="product-presets">
+                                            <button className="remove"> lixo</button>
+                                        </div>
+                                </div>
+                            </div>
                         </div>
                     }
                 />                     
--- src/pages/Menu/components/CartModal/style.ts
+++ src/pages/Menu/components/CartModal/style.ts
@@ -2,14 +2,48 @@ import { styled } from "styled-components";
 
 export const Container = styled.div`
     .cart-modal {
-        position: absolute;
-        top: 0;
         right: 0;
         width: 140px;
         height: 100vh;
         border: 1px solid #e0e0e0;
         border-top-left-radius: 4px;
         background: #fff;
+        padding: 8px;
+        padding-top: 10px;
+
+        display: flex;
+        flex-direction: column;
+        text-align: center;
     }
 
+    .cart-m-total-cost {
+        margin-bottom: 5px;
+        span {
+            font-size: 1.2rem;
+        }
+        h2 {
+            color: #b32704;
+        }
+    }
+
+    .free-delivery {
+        line-height: 1.3;
+        p {
+            font-size: 1.1rem;
+            color: gray;
+        }
+        #green-p {
+            color: #067d62;
+        }
+        #link-p {
+            text-decoration: none;
+            color: #007185;
+
+        }
+
+        #link-p:hover {
+            text-decoration: underline;
+            color: var(--link-color-hover);
+        }
+    }
 `;
